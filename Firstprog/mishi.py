cfece2a51cbd01060de1af44a27af28a79b18b29
midhilajnisam1 <56074145+midhilajnisam1@users.noreply.github.com>
2019-10-07 18:04:11 +0530

    Add files via upload

---
+++ Firstprog/mishi.py
@@ -0,0 +1,11 @@
+dict1 = {"Burger":"Best fast food for timepass", "PS4":"A Gaming Console for Game Lovers", "Midhilaj":"Best in the world","Avengers":"Best multi super hero movie ever",2:"Lucky Number for Most peoples" }
+print("""Enter any of the words below to know the dumbest definitions\n
+1. Burger\n
+2. PS4\n
+3. Midhilaj\n
+4. Avengers\n
+5. 2\n
+""")
+user_input= input("Enter your selected word from the list:\n")
+print(dict1[user_input])
+print("Thank You for using Dumbest Dictionary Ever!!")
